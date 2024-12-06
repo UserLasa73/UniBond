679770910290973c090760233fc915297608995d
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2024-12-06 14:53:27 +0530

    changing the background color

--- frontend/app/Components/Project_topbar.tsx
+++ frontend/app/Components/Project_topbar.tsx
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     paddingHorizontal: 16,
     height: 60,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: '#ffffff',
   },
   button: {
     width: 40,
