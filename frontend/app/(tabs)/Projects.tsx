fa5ca5b2f8b9b14e22b4e708386f9e49c6c53dc0
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2024-12-06 14:01:02 +0530

    feat: calling Project_topbar component in Projects

--- frontend/app/(tabs)/Projects.tsx
+++ frontend/app/(tabs)/Projects.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import { Text, View, StyleSheet } from "react-native";
+import Project_topbar from '../Components/Project_topbar';
 
 const Projects = () => {
   return (
     <View style={styles.container}>
-      <Text>Projects</Text>
+      {/* <Text>Projects</Text> */}
+      <Project_topbar/>
     </View>
   );
 };
