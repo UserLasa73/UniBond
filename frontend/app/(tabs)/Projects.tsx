5601154692c2f1bbe7d17987e71f854372ca1e30
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2024-12-06 14:54:43 +0530

    feat: import and calling Project_conditionbar component in Projects

--- frontend/app/(tabs)/Projects.tsx
+++ frontend/app/(tabs)/Projects.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import { Text, View, StyleSheet } from "react-native";
 import Project_topbar from '../Components/Project_topbar';
+import Project_conditionbar from '../Components/Project_conditionbar'
 
 const Projects = () => {
   return (
     <View style={styles.container}>
       {/* <Text>Projects</Text> */}
       <Project_topbar/>
+      <Project_conditionbar/>
     </View>
   );
 };
