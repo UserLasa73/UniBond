4dad1a9cc33b01065bc8c94085a5992bf4e02718
aathif12 <aathifahamad4@gmail.com>
2024-12-13 15:23:03 +0530

    Clear The Running Errors

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,8 +1,7 @@
-module.exports = {
-  presets: [
-    "module:metro-react-native-babel-preset", // Preset for React Native projects
-  ],
-  plugins: [
-    "react-native-reanimated/plugin", // Keep this for React Native Reanimated
-  ],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: ["react-native-reanimated/plugin"],
+  };
 };
