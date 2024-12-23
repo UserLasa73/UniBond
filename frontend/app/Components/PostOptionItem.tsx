2d8936737fdf5cbe53f8e535010aa95fb92fe1a0
Jellorine <jjello1356@gmail.com>
2024-12-23 10:50:21 +0530

    post options component created

---
+++ frontend/app/Components/PostOptionItem.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet, View } from "react-native";
+
+const PostOptionItem = ({ label, icon, onPress }: any) => {
+  return (
+    <TouchableOpacity style={styles.item} onPress={onPress}>
+      <View style={styles.icon}>{icon}</View>
+      <Text style={styles.label}>{label}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default PostOptionItem;
+
+const styles = StyleSheet.create({
+  item: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  label: {
+    fontSize: 16,
+    color: "#333",
+  },
+});
