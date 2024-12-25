778c841673947f83f95f20e91c4854e5bba15989
aathif12 <aathifahamad4@gmail.com>
2024-12-25 08:20:39 +0530

    Two WayChat

--- frontend/app/Components/UserList.tsx
+++ frontend/app/Components/UserList.tsx
@@ -1,15 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
+import { useChatContext } from "stream-chat-expo";
+import { useAuth } from "../providers/AuthProvider";
+import { router } from "expo-router";
 
 const UserList = ({ user }) => {
+  const { client } = useChatContext();
+  const { user: me } = useAuth();
+  const onPress = async () => {
+    const channel = client.channel("messaging", {
+      members: [me?.id, user.id],
+    });
+    await channel.watch();
+    router.replace(`/(home)/channel/${channel.cid}`);
+  };
   return (
-    <View
+    <Pressable
+      onPress={onPress}
       style={{
         padding: 15,
         backgroundColor: "#fff",
       }}
     >
       <Text style={{ fontWeight: "bold" }}>{user.full_name}</Text>
-    </View>
+    </Pressable>
   );
 };
 export default UserList;
