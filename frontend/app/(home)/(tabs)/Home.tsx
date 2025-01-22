2df883b9d19b434679c82c17f0f2ac9447f4881d
aathif12 <aathifahamad4@gmail.com>
2025-01-22 11:36:40 +0530

    Add Donate Button

--- frontend/app/(home)/(tabs)/Home.tsx
+++ frontend/app/(home)/(tabs)/Home.tsx
@@ -6,13 +6,16 @@ import {
   FlatList,
   ActivityIndicator,
   Text,
+  TouchableOpacity,
+  Image,
 } from "react-native";
 import { useRouter } from "expo-router";
 import { useAuth } from "@/app/providers/AuthProvider";
 import TopNavigationBar from "../../Components/TopNavigationBar";
 import { supabase } from "../../../lib/supabse";
 import PostItem from "../../screens/PostItem";
-
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 type Post = {
   id: number;
   content: string;
@@ -33,7 +36,7 @@ type Event = {
 const HomeScreen: React.FC = () => {
   const router = useRouter();
   const { session } = useAuth();
-
+  const navigation = useNavigation();
   const [username, setUsername] = useState<string>("");
   const [combinedData, setCombinedData] = useState<(Post | Event)[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
@@ -211,11 +214,43 @@ const HomeScreen: React.FC = () => {
         }
         contentContainerStyle={styles.combinedList}
       />
+      <TouchableOpacity
+        style={styles.DonateButton}
+        onPress={() => {
+          router.push("/screens/DonationScreen");
+        }}
+      >
+        <Image source={require("../../Constatnts/Donate Icon.png")} />
+        <Text style={{ color: "#000", fontWeight: "bold" }}>Donate</Text>
+      </TouchableOpacity>
     </>
   );
 };
 
 const styles = StyleSheet.create({
+  DonateButton: {
+    borderWidth: 1,
+    borderColor: "#EBF2FA",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 70,
+    position: "absolute",
+    top: 600, // Consider replacing this with a more responsive positioning like `bottom`.
+    right: 20,
+    height: 70,
+    backgroundColor: "#EBF2FA",
+    borderRadius: 100,
+    // Shadow for iOS
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    // Shadow for Android
+    elevation: 5,
+  },
   combinedList: {
     padding: 16,
   },
