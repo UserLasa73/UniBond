1a3cb90536d20e86eae13a09e996e63ccb396cf4
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2025-03-16 02:16:58 +0530

    Refactor Projects component to use SafeAreaView and improve layout styling

--- frontend/app/(home)/(tabs)/Projects.tsx
+++ frontend/app/(home)/(tabs)/Projects.tsx
@@ -1,22 +1,31 @@
 import * as React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, SafeAreaView } from "react-native";
 import Project_topbar from "../../Components/Project_topbar";
 import Project_conditionbar from "../../Components/Project_conditionbar";
-import Project_titlebox from "../../Components/Project_titlebox";
+//import Project_titlebox from "../../Components/Project_titlebox";
 
 const Projects = () => {
   return (
-    <View style={styles.container}>
-      {/* <Text>Projects</Text> */}
-      <Project_topbar />
-      <Project_conditionbar />
-      {/* <Project_titlebox /> */}
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        {/* <Text>Projects</Text> */}
+        <Project_topbar />
+        <Project_conditionbar />
+        {/* <Project_titlebox /> */}
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default Projects;
 
 const styles = StyleSheet.create({
-  container: {},
-});
+  container: {
+    flex: 1,
+  },
+  safeArea:{
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+}
+);
